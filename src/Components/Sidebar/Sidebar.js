e189d3827c76a7fb62b8b009090015abf29fc268
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2020-04-29 01:15:22 +0530

    added client side nickname

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Sidebar.css";
 function Sidebar(props){
+    let userNickName = sessionStorage.getItem('nick');
+    if(userNickName == null) userNickName = "user";
+    
     return(
         <div className="main-sidebar">
             <div className="user-title">
-                <h4>Hi bluepie</h4>
+                <h4>Hi {userNickName}</h4>
             </div>
             <div className="user-content">
                 <ul className="user-menuitems">
