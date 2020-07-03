f200ad4b7eda3f16312e00e42f24ae6afc764c1a
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2020-07-04 05:09:04 +0530

    utilize snackbar inside details popup

--- src/Components/FileManager/FileInfo.js
+++ src/Components/FileManager/FileInfo.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./FileInfo.css";
+import {SnackBar} from "../SnackBar/Snackbar";
 
 function FileInfo(props){
-    console.log(props);
+    const [snack, setSnack] = useState(false);
     return (
         <div className="ui modal">
-            <i class="close icon"></i>
-            <div class="header">File info</div>
-            <div class="content center-txt">
+            <i className="close icon"></i>
+            <div className="header">File info</div>
+            <div className="content center-txt">
                 <span>file : {props.property.key}</span>
                 <br></br>
                 <span>download Id : not generated</span>
                 <br></br>
             <span>
-                <div class="ui approve button">generate download id</div>
+                <div className="ui approve button" onClick={()=>setSnack(true)}>generate download id</div>
             </span>
             </div>
+            {snack && <SnackBar params={{show:true,key:props.property.key,cb:setSnack}}/>}
         </div>
     );
 }
