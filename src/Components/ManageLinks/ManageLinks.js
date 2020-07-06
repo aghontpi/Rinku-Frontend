43416f87948e632dda948c74c4375ed8b20a7223
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2020-07-07 03:04:02 +0530

    add prev next for navigating in list

--- src/Components/ManageLinks/ManageLinks.js
+++ src/Components/ManageLinks/ManageLinks.js
@@ -2,6 +2,7 @@ import React,{useEffect, useState} from "react";
 import style from "./style.module.css";
 import { managelinks } from "./../../Api/ManageLinks"
 import {SnackBar} from "../SnackBar/Snackbar";
+import {usePrevious} from "../Utils";
 function ManageLinks(props){
     const [contents, setContents] = useState({items:10,content:null})
     const [snack, setSnack] = useState(false);
@@ -9,8 +10,8 @@ function ManageLinks(props){
         msg:"",
         type:"success"
     });
-
-    useEffect(()=>{
+    const prevcontent = usePrevious(contents);
+    const apiCall = () => {
         managelinks({limit:contents.items})
             .then((response)=>  (response.status === 200) ? response.json() : {})
             .then((json)=>{
@@ -23,8 +24,10 @@ function ManageLinks(props){
                             });
                             setSnack(true)
                         }
-                        console.log(list);
-                        list ? setContents({limit:contents.item,content:list}) : err();
+                        list ? setContents((prevstate) => {
+                                    return {items:prevstate.items,content:list}
+                                }) 
+                            : err();
                     } else {
                         const  err = json.response 
                             ? json.errors.errMsg && json.errors.errMsg
@@ -36,15 +39,25 @@ function ManageLinks(props){
                         setSnack(true);
                     }
                 })
+    }
+
+
+    useEffect(()=>{
+        apiCall();
     },[])
+
+    useEffect(()=>{
+        console.log(contents)
+        prevcontent && (contents.items !== prevcontent.items) && apiCall();
+    },[contents])
 
     return (
         <div className="linksholder">
             <div>Links Available</div>
-            { contents.content !== null && list(contents) }
+            { contents.content !== null && list(contents,setContents) }
             {snack && <SnackBar params={
                 {show:true,
-                key:props.property.key,
+                key:1,
                 msg:snackProp.msg,
                 type:snackProp.type,
                 cb:setSnack}
@@ -53,7 +66,26 @@ function ManageLinks(props){
     );
 }
 
-function list({item, content}){
+function list({items, content},setContents){
+
+    const modifyItems = (value)=> {
+        value && setContents((prevstate)=>{
+            const itemsChange = (+prevstate.items) + (+value) 
+            console.log(prevstate,itemsChange);
+            return{
+                ...prevstate,
+                items: itemsChange,
+            }
+        });
+    }
+
+    const nextBtnClick = () => {
+        modifyItems(+10)
+    };
+    const prevBtnClick = () => {
+        modifyItems(-10)
+    }
+
     return(
         <table className="ui stripped table">
             {
@@ -71,7 +103,7 @@ function list({item, content}){
                 })
             }
             {
-                tableFooter()
+                tableFooter({items:items,count:content.length},{prev:prevBtnClick,next:nextBtnClick})
             }
         </table>
     );
@@ -89,15 +121,17 @@ function tableHeader(){
     );
 }
 
-function tableFooter(){
+function tableFooter({items,count},{prev,next}){
+    const prevBtnCls = "ui small button " + (items === 10 ? " disabled " : "");
+    const nextBtncls = "ui small right floated button" + (count < 10 ? " disabled " : "");
     return(
         <tfoot className="full-width">
             <tr>
                 <th colSpan="4">
-                    <div className="ui small button disabled" >
+                    <div className={ prevBtnCls} onClick={prev} >
                         prev
                     </div>
-                    <div className="ui small right floated button">
+                    <div className={ nextBtncls} onClick={next}>
                         next
                     </div>
                 </th>
