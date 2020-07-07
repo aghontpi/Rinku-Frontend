f372dac173ccda6e2a8c2c09c6b5a3f67ccade04
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2020-07-08 03:36:26 +0530

    server: move configs into /interfaces/config.php

--- server/classes/database.php
+++ server/classes/database.php
@@ -4,16 +4,24 @@
  */
 namespace server\classes;
 
+include_once __DIR__."/../interfaces/config.php";
+
 use PDO;
 use PDOException;
+use \server\interfaces\config as Iconfig;
 
-class database{
+class database implements Iconfig{
     static $connection = null;
 
     public static function getConnection() {
         try{
             if(is_null(self::$connection))
-                self::$connection =new PDO("mysql:host=database;dbname=backend_db;","user","user");
+                self::$connection =new PDO(
+                    "mysql:host=".database::host.";"
+                        ."dbname=".database::database.";",
+                    database::user,
+                    database::password
+                );
         }
         catch (PDOException $e){
             die($e->getMessage());
--- server/interfaces/config.php
+++ server/interfaces/config.php
@@ -5,6 +5,10 @@ namespace server\interfaces;
 interface config{
     /* tells application the root path to operate on */
     const path = ".";
+    const host = "database";
+    const database = "backend_db";
+    const user = "user";
+    const password = "user";
 }
 
 ?>
