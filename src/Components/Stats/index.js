b374bdec02efdc99ea75ac0fc1727293eabb09a8
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2020-07-08 01:18:17 +0530

    client-react: integrate server data to client side

--- src/Components/Stats/index.js
+++ src/Components/Stats/index.js
@@ -1,17 +1,48 @@
 import React,{useEffect, useState} from "react";
 import {SnackBar} from "../SnackBar/Snackbar";
-import {Chart} from "./Barchar"
+import {Chart} from "./Barchar";
+import {Stats as Sapi} from "../../Api/Stats";
+
 function Stats(){
     const [snack, setSnack] = useState(false);
     const [snackProp, setSnackProp] = useState({
         msg:"",
         type:"success"
     });
+    const [stats,setStats] = useState(null);
+    useEffect(()=>{
+        Sapi({date:"datehere"})
+            .then((response)=>  (response.status === 200) ? response.json() : {})
+            .then((json)=>{
+                if(json.response !== "success"){
+                    const  err = json.response 
+                                ? json.errors.errMsg && json.errors.errMsg
+                                : "can not fetch list from server"
+                    setSnackProp({
+                            msg:err,
+                            type:"error"
+                        });
+                    setSnack(true);
+                    return;
+                }
+                const stats = JSON.parse(json.content.stats);
+                const err = ()=>{
+                    setSnackProp({
+                        msg:'error fetching stats from server',
+                        type:'success'
+                    });
+                    setSnack(true)
+                }
+                stats ? setStats(stats) : err();  
+
+            })
+    },[])
+
     return(
-        <div className="linksholder">
-            <div>stats</div>
+        <div className="chartHolder">
+            <div>Dowload Stats</div>
             <div style={{height:"600px"}}>
-            <Chart/>
+            <Chart stats={stats}/>
             </div>
             
             
