e0d1e2130034251dadda6c3cba657da0a9ec2ad7
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2020-07-09 01:45:18 +0530

    client: css-transition for react-router

--- src/Components/PageHolder/PageHolder.js
+++ src/Components/PageHolder/PageHolder.js
@@ -1,36 +1,51 @@
 import React from "react";
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route, useLocation} from "react-router-dom";
 import "./PageHolder.css";
 import PageHeader from "./../PageHeader/PageHeader";
 import FadeIn from "react-fade-in";
+import {
+    TransitionGroup,
+    CSSTransition
+  } from "react-transition-group";
 
 
 const PageHolder = (props) => {
+    let windowLocation = useLocation();
     return(
         <div className="page-holder">
         <PageHeader />
-            
+
                 <div className="actual-page">
-                <FadeIn delay="100" transitionDuration="800">
-                    <div className="title-holder">
-                        <div className="page-title">
+                    <FadeIn delay="100" transitionDuration="800">
+                        <div className="title-holder">
+                            <div className="page-title">
+                            </div>
                         </div>
-                    </div>
-                    <Switch>
-                        { 
-                            props.routes.map((routes, index) => {
-                                return(
-                                <Route
-                                    key={index}
-                                    path={routes.path}
-                                    exact={routes.exact}
-                                    children={routes.children}
-                                />
-                                )
-                            })
-                            
-                        }
-                    </Switch>
+                            <TransitionGroup>
+                                <CSSTransition
+                                    key={windowLocation.key}
+                                    classNames="routeTransition"
+                                    timeout={{ enter: 300, exit: 300 }}
+                                >
+                                <div className="page-render-area">
+                                    <Switch>
+                                        { 
+                                            props.routes.map((routes, index) => {
+                                                return(
+                                                <Route
+                                                    key={index}
+                                                    path={routes.path}
+                                                    exact={routes.exact}
+                                                    children={routes.children}
+                                                />
+                                                )
+                                            })
+                                            
+                                        }
+                                    </Switch>
+                                </div>
+                                </CSSTransition>
+                            </TransitionGroup>
                     </FadeIn>
                 </div>
         </div>
