b1e907cff140ff91f6a49ae13e000f6c5b5cac47
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2020-07-09 01:40:19 +0530

    client add a random interval of 450ms to 1 second

--- src/Api/Fetch.js
+++ src/Api/Fetch.js
@@ -1,5 +1,6 @@
 
-const Fetch = (params) => {
+const Fetch = async (params) => {
+    await new Promise(resolve => setTimeout(resolve, Math.random()*(1000 - 450) + 450));
     return fetch('http://localhost/index.php', {
         method: 'POST',
         headers: {'Content-Type':'application/json'},
