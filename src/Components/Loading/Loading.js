6e750c1f71793e28160cac3321660fffe138b676
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2020-07-09 01:44:42 +0530

    client: update the loading screen to include animation.

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,24 +1,30 @@
 import React from "react";
 import Lottie from "react-lottie";
 import ReactDOM from "react-dom";
-import ReactLoading from "react-loading";
 import "./loading.css";
+import * as cLoading from "./circle-loading.json"
+import FadeIn from "react-fade-in";
 
 const Loading = (props) => {
-    // const content = 
-    //     props && props.show ?
-    //         <div className="loading-container">
-    //             <div className="loading-contents">
-    //                 <ReactLoading type={"bubbles"} color={"black"} />
-    //             </div>
-    //         </div>
-    //         : "";
-    let content = 
+    const options = {
+        loop: true,
+        autoplay: true,
+        animationData: cLoading.default,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+    const content = 
+        props && props.show ?
             <div className="loading-container">
                 <div className="loading-contents">
-                    <ReactLoading type={"bubbles"} color={"black"} />
+                    <FadeIn>
+                        <Lottie options={options} width="400px" height="400px" speed="1.5"/> 
+                    </FadeIn>
                 </div>
+                
             </div>
+            : "";
     
         
     return ReactDOM.createPortal(content,document.getElementsByTagName('body')[0]);
