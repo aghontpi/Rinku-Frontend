763e345944e3f51c0af9338983329c896cfb94ba
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-05-28 02:36:43 +0530

    :syringe: refactor pageheader

--- src/Components/PageHolder/PageHolder.js
+++ src/Components/PageHolder/PageHolder.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Switch, Route, useLocation, BrowserRouter, Redirect } from "react-router-dom";
 import "./PageHolder.css";
-import PageHeader from "./../PageHeader/PageHeader";
+// import PageHeader from "./../PageHeader/PageHeader";
 import FadeIn from "react-fade-in";
 import {
     TransitionGroup,
     CSSTransition
 } from "react-transition-group";
+import { PageHeader } from "../../Component/PageHeader";
 
 
 const PageHolder = (props) => {
     let windowLocation = useLocation();
     return (
         <div className="page-holder">
-            <PageHeader location={windowLocation} />
+            <PageHeader />
 
             <div className="actual-page">
                 <FadeIn delay="100" transitionDuration="800">
