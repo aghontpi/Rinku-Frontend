20ecabe17c7ccd531e00ee9f4b07eba1912f3af4
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-05-29 01:35:49 +0530

    :syringe: refactor sidebar

--- src/Containers/index.ts
+++ src/Containers/index.ts
@@ -1,1 +1,5 @@
 export { Download } from './Download';
+export { FileManager } from './FileManager';
+export { ManageLinks } from './ManageLinks';
+export { Stats } from './Stats';
+export { DownloadLog } from './DownloadLog';
--- src/Route/Routes.tsx
+++ src/Route/Routes.tsx
@@ -1,29 +1,12 @@
-import React, { ReactNode, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Redirect, Route as RRoute, Switch } from 'react-router-dom';
-import { DownloadLog } from '../Components/DownloadLog/DownloadLog';
-import Home from '../Components/Home/Home';
-import ManageLinks from '../Components/ManageLinks/ManageLinks';
-import { Stats } from '../Components/Stats';
+import Sidebar from '../Component/Sidebar/Sidebar';
+
 import { Download } from '../Containers/';
 import { Login } from '../Containers/Login';
 import { useAppDispatch } from '../Hooks/app.hook';
 import { useAuth } from '../Hooks/authentication.hook';
 import { hideLoaderAction } from '../Store/loader.store';
-
-interface RouteConfig {
-  path: string;
-  component: ReactNode;
-}
-
-const ROUTE_PATHS: RouteConfig[] = [
-  { path: '/home', component: Home },
-  {
-    path: '/manage-links',
-    component: <ManageLinks />,
-  },
-  { path: '/stats', component: <Stats /> },
-  { path: '/download-logs', component: <DownloadLog /> },
-];
 
 export const Route = () => {
   const auth = useAuth();
@@ -59,13 +42,12 @@ const PostAuth = () => {
   return (
     <Router>
       <Switch>
-        {ROUTE_PATHS.map(({ component, path }, index) => (
-          <RRoute exact {...{ path }} component={component as any}></RRoute>
-        ))}
+        <RRoute path="/home">
+          <Sidebar />
+        </RRoute>
         <RRoute path="/download/:fileid">
           <Download />
         </RRoute>
-
         <Redirect from="*" to="/home" />
       </Switch>
     </Router>
