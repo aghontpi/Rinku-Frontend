0c90918a755c3d8ea820a23e654a016ac79afda5
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-05-29 03:14:31 +0530

    :pencil2: refactor authentication hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import './App.css';
 import { Loader } from './Component/Loading';
 import { Route } from './Route';
 import { store } from './Store';
+
+const history = createBrowserHistory();
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <Loader />
-        <Route />
-      </Provider>
+      <Router history={history}>
+        <Provider store={store}>
+          <Loader />
+          <Route />
+        </Provider>
+      </Router>
     </>
   );
 }
